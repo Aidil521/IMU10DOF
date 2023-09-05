d6893fc9214b2d0bf9e10819f33cb3040ce82ac4
Aidil011215 <aidilmuhamad151201@gmail.com>
2023-09-05 22:03:23 +0700

    first commit

--- src/IMU10DOF.h
+++ src/IMU10DOF.h
@@ -44,14 +44,15 @@ class IMUSensor{
 	void setAccOffsets(float x, float y, float z);
 	void calcOffsets(bool _offsetMPU = true, bool _offsetQMC = true, bool _offsetBMP = true);
 
+    // Call variabel used for calibration sensor MPU6050
 	float getGyroXoffset(){ return gyroXoffset; };
     float getGyroYoffset(){ return gyroYoffset; };
     float getGyroZoffset(){ return gyroZoffset; };
-	
 	float getAccXoffset(){ return accXoffset; };
 	float getAccYoffset(){ return accYoffset; };
 	float getAccZoffset(){ return accZoffset; };
 
+    //Call variabel for value measurement
     float getAngleX(){ return angleX; };
     float getAngleY(){ return angleY; };
     float getAngleZ(){ return angleZ; };
@@ -61,12 +62,13 @@ class IMUSensor{
     float getTemp(){ return temp / 1.3;};
     float getPressure(){ return Pressure;}
 
+    // Call function for update value measurement IMU Sensors
     void update();
 
   private:
-	void calcDataMPU(); // user should better call 'update' that 
-	void calcDataQMC(); // user should better call 'update' that 
-	void calcDataBMP(); // user should better call 'update' that 
+	void calcDataMPU(); // user should better call function for update measurement sensors MPU6050
+	void calcDataQMC(); // user should better call function for update measurement sensors MPU6050 
+	void calcDataBMP(); // user should better call function for update measurement sensors MPU6050
 	uint8_t writeData(uint8_t add, uint8_t reg, uint8_t data);
     uint8_t readByte(uint8_t add, uint8_t reg);
     void readMPU(uint8_t reg, int bitData);
